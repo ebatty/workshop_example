33b559b090e4cc5c2e51a9cf5e06072f5e4e9dcc
ebatty <erb2180@columbia.edu>
2022-01-20 08:40:15 -0500

    updates

---
+++ another_dir/overview.py
@@ -0,0 +1,7 @@
+
+# import sys
+# sys.path.append('/Users/eleanorbatty/workshop_example/')
+
+from utils.util import my_function, second_function
+
+my_function()
---
+++ test3.py
@@ -0,0 +1,5 @@
+#from utils.util import my_function, second_function
+import utils.util 
+# #from utils.util import *
+
+utils.util.my_function()
