9d71cf26b0bab7f66ac2a51d67b1d92d1f13df69
Ella Batty <erb2180@columbia.edu>
2022-01-20 10:02:45 -0500

    Update and rename test.py to main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+np.random.seed(0)
+for i in np.random.randint(100, size=(3,)):
+    print(i)
--- test.py
+++
@@ -1,8 +0,0 @@
-def my_function(a):
-  
-    qq = [1, 2, 3, 4]
-    for ii in qq:
-        qq[ii] += 1
-
-    return qq
-
